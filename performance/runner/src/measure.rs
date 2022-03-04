e58e155de74d3cf232c8b3f0a4ff3044f2927b36
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-03-03 19:17:42 -0500

    run two samples just for now

--- performance/runner/src/measure.rs
+++ performance/runner/src/measure.rs
@@ -145,7 +145,8 @@ pub fn take_samples(projects_dir: &PathBuf, out_dir: &PathBuf) -> Result<Vec<Sam
         let mut output_file = out_dir.clone();
         output_file.push(metric.filename());
 
-        let status = run_hyperfine(&path, &command, hcmd.clone().prepare, 1, &output_file)
+        // TODO we really want one run, not two. so we might not want to use hyperfine for taking samples.
+        let status = run_hyperfine(&path, &command, hcmd.clone().prepare, 2, &output_file)
             .or_else(|e| Err(RunnerError::from(e)))?;
 
         match status.code() {
